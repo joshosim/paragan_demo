2101f3724eddbadf29bfaeaec8ad622ec6d74ae3
osimuka <osimukaokpan@gmail.com>
2024-04-04 09:36:32 +0100

    more

--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@ const submitLogin = () => {
 const submitSignup = () => {
   window.location.href = "home.html";
 };
+
+const loginBtn = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signupBtn");
+const login = document.getElementById("login");
+const signup = document.getElementById("signup");
+
+loginBtn.addEventListener("click", () => {
+  login.classList.remove("hidden");
+  signup.classList.add("hidden");
+});
+signupBtn.addEventListener("click", () => {
+  login.classList.add("hidden");
+  signup.classList.remove("hidden");
+});
